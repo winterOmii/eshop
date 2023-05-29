8984ae4db6b67c03a4603cd1937814c2ada6b302
Yfd_omibeaver <108644167+winterOmii@users.noreply.github.com>
2023-05-29 19:18:39 +0800

    up

--- release.js
+++ release.js
@@ -6,8 +6,8 @@
 
 const options = {};
 
-// options.ip = '127.0.0.1';
-// options.port = parseInt(process.argv[2]);
+options.ip = '127.0.0.1';
+options.port = parseInt(80);
 // options.config = { name: 'Total.js' };
 // options.sleep = 3000;
 
