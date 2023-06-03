ccd80affc1cec40b9d72a1152d0073c3d1832709
Yfd_omibeaver <108644167+winterOmii@users.noreply.github.com>
2023-06-03 11:54:03 +0800

    push

--- release.js
+++ release.js
@@ -6,8 +6,8 @@
 
 const options = {};
 
-options.ip = '127.0.0.1';
-options.port = parseInt(80);
+// options.ip = '127.0.0.1';
+// options.port = parseInt(80);
 // options.config = { name: 'Total.js' };
 // options.sleep = 3000;
 
